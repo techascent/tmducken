76e05c8dbce9bcf46c6a6d6400712db79dd420be
Chris Nuernberger <chris@techascent.com>
2023-08-09 14:25:11 -0600

    tools.build based deploy

---
+++ build.clj
@@ -0,0 +1,27 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [clojure.edn :as edn])
+  (:refer-clojure :exclude [compile]))
+
+(def deps-data (edn/read-string (slurp "deps.edn")))
+(def codox-data (get-in deps-data [:aliases :codox :exec-args]))
+(def lib (symbol (codox-data :group-id) (codox-data :artifact-id)))
+(def version (codox-data :version))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s.jar" (name lib)))
+(def uber-file (format "target/uber-%s.jar" (name lib)))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
