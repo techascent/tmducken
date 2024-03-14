c78a9c889161fb3d8b1535ce751bc0c0f5824a36
Craig Brozefsky <129115916+craig-latacora@users.noreply.github.com>
2024-03-14 12:14:48 -0400

    Update nixpkgs input to be nixos-unstable to get 0.10.0 duckdb (#27)

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
-  description = "tmducken - tech.ml.dataset Integration with DuckDB for Clojure";
+  description =
+    "tmducken - tech.ml.dataset Integration with DuckDB for Clojure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     clj-nix.url = "github:jlesquembre/clj-nix";
   };
@@ -15,31 +16,29 @@
         # Simulating JNA Platform#getNativeLibraryResourcePrefix()
         # Flip arch-os from Nix to os-arch
         system-parts = pkgs.lib.splitString "-" pkgs.system;
-        os-arch-prefix-underscores = builtins.concatStringsSep "-" (pkgs.lib.reverseList system-parts);
+        os-arch-prefix-underscores =
+          builtins.concatStringsSep "-" (pkgs.lib.reverseList system-parts);
         # NOTE: ^ this contains x86_64, have to fix to be equivalent to JNA
-        os-arch-prefix = builtins.replaceStrings [ "_" ] [ "-" ] os-arch-prefix-underscores;
+        os-arch-prefix =
+          builtins.replaceStrings [ "_" ] [ "-" ] os-arch-prefix-underscores;
         lib-folder = "./lib/${os-arch-prefix}";
         # Definining a reusable DLL providing script, used for Nix repeatable build AND direnv shell entering (development)
         postPatch = ''
           mkdir -p ${lib-folder}
           ln -sf ${pkgs.duckdb}/lib/* ${lib-folder}
         '';
-      in
-      {
+      in {
 
         packages = {
           duckdb = pkgs.duckdb;
-          default =
-            let version = pkgs.duckdb.version;
-            in
-            cljpkgs.mkCljLib
-              {
-                inherit postPatch version;
-                projectSrc = ./.;
-                name = "com.techascent/tmducken";
-                buildCommand = "clj -T:build jar --report stderr";
-                preBuild = "export DUCKDB_VERSION=${version}";
-              };
+          default = let version = pkgs.duckdb.version;
+          in cljpkgs.mkCljLib {
+            inherit postPatch version;
+            projectSrc = ./.;
+            name = "com.techascent/tmducken";
+            buildCommand = "clj -T:build jar --report stderr";
+            preBuild = "export DUCKDB_VERSION=${version}";
+          };
         };
         devShells.default = pkgs.mkShell {
           buildInputs = [
